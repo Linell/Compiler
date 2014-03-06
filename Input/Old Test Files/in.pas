154827b2036ce51533d9e7594cf07cd853ba11e8
Linell <timothy.bonnette@eagles.usm.edu>
2014-03-06 12:33:46 -0600

    Not sure what's happening

---
+++ Input/Old Test Files/in.pas
@@ -0,0 +1,27 @@
+CONST
+
+        A=10;
+
+VAR
+
+        B1,C;
+
+
+
+PROCEDURE ONE;
+
+CONST
+
+        A=5;
+
+VAR
+
+        C1,D1;
+
+BEGIN
+
+        D1:=30;
+
+        FOR B1:=10 DOWNTO A DO
+
+Semicolon or end expected. 
